5e0cb3d60b753de31c7240b027386f07ff0ab75c
xnought <bertuccd@oregonstate.edu>
2021-02-15 15:30:15 -0800

    Cleaned up and started implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+/* 
+  Donny Bertucci: @xnought
+  Date Created: 02/15/2021
+  Summary: 
+    This file acts as the highest state and act as the controls 
+    for the entire application
+*/
+import React, { Component } from "react";
+import { Typography } from "@material-ui/core";
+import PlayButton from "./components/PlayButton";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		/* Treat the app state as the global state */
+		this.state = {
+			/* data: stores the input and lables to the input */
+			data: {
+				X: [],
+				y: [],
+			},
+			/* Stores the model and model metadata */
+			model: {
+				neurons: [],
+				shape: [],
+				loss: null,
+				yhat: null,
+			},
+			/* Stores the controls */
+			controls: {
+				playing: false,
+			},
+		};
+
+		/* Functions Binds to This: Prototype */
+		this.mutatePlaying = this.mutatePlaying.bind(this);
+	}
+
+	/* 
+    Name: mutatePlaying
+    Purpose: change playing to !playing, i.e true to false, or false to true
+    @mutate: this.state.controls.playing 
+  */
+	mutatePlaying() {
+		const { controls } = this.state;
+		const playing = !controls.playing;
+		this.setState({
+			...this.state,
+			controls: { ...controls, playing },
+		});
+	}
+
+	/* Create a function that initializes the values for the Nerual network */
+	/* Create a function that constructs the or perhaps put this in a component */
+	/* Create a function that performs the forward pass */
+	/* Create a function that performs the backward pass */
+	/* Create a function that performs the update sgd */
+
+	render() {
+		/* Destructuring */
+		const { data, model, controls } = this.state;
+
+		/* Making return less complex */
+		const PlayButtonClick = (
+			// eslint-disable-next-line
+			<a onClick={this.mutatePlaying}>
+				<PlayButton playing={controls.playing} />
+			</a>
+		);
+
+		/* rendered to virtual DOM */
+		return (
+			<div>
+				<Typography variant="h2">Nice!</Typography>
+				{PlayButtonClick}
+			</div>
+		);
+	}
 }
 
 export default App;
